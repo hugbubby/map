472d64aff452b07f4f83d5275a41a1943647426c
test <test@test.com>
2019-10-29 15:24:43 -0500

    Initialidad

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hugbubby/map
 
 go 1.13
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -1,68 +1,86 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
-)
 
-func clog(msg ...interface{}) {
+	flag "github.com/spf13/pflag"
+)
+var quiet bool
+
+func clog(msg string) {
 	log.Println("map: ", msg)
 }
 
 func runcmd(cmd_s string, done chan struct{}) {
 	cmd := exec.Command("/bin/bash", "-c", cmd_s)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
+	if !quiet {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+	}
 	if err := cmd.Run(); err != nil {
-		clog(err)
+		clog(err.Error())
 	}
 	done <- struct{}{}
 }
 
-var threads int
-
 func main() {
-	var replacement, delimiter string
-	flag.StringVar(&replacement, "r", "_MAP_", "The string to replace in your command with the array's members.")
-	flag.StringVar(&delimiter, "d", " ", "The delimiter for your array.")
-	flag.IntVar(&threads, "t", 1, "The number of threads to use for cmd replacement; default 1. Currently unimplemented.")
+	var wordlists []string
+	var threads int
+	var replacename, delimiter string
+
+	flag.StringArrayVarP(&wordlists, "wordlist", "w", []string{}, "The wordlist to run commands from. Can be used multiple times to specify multiple wordlists.")
+	flag.StringVarP(&replacename, "replacename", "r", "GMAP", "The string to replace in your command with the array's members.")
+	flag.StringVarP(&delimiter, "delimiter", "d", " ", "The delimiter for your array.")
+	flag.IntVarP(&threads, "threads", "t", 1, "The number of threads to use for cmd replacement; default 1.")
+	flag.BoolVarP(&quiet, "quiet", "q", false, "Whether or not to quiet output.")
+
 	flag.Parse()
 
 	delimiter = strings.ReplaceAll(delimiter, "\\n", "\n")
 	delimiter = strings.ReplaceAll(delimiter, "\\t", "\t")
 	delimiter = strings.ReplaceAll(delimiter, "\\r", "\r")
 
-	if flag.NArg() != 2 {
-		fmt.Println("Incorrect number of arguments.")
+	if flag.NArg() == 0 {
+		fmt.Println("Must specify entire comamnd to run in the single argument given to his program!")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
-
-	array := strings.Split(flag.Arg(0), delimiter)
-	cmd_s := flag.Arg(1)
-
-    //Small Queue.
-	done := make(chan struct{}, threads)
-    numJobs := len(array)
-    jobsFinished := 0
-    for i := 0; i < threads && len(array) > 0; i++ {
-        cmd_replaced := strings.ReplaceAll(cmd_s, replacement, array[0])
-        go runcmd(cmd_replaced, done)
-        array = array[1:]
-    }
-	for len(array) > 0 {
-        <-done
-        jobsFinished++
-        cmd_replaced := strings.ReplaceAll(cmd_s, replacement, array[0])
-        go runcmd(cmd_replaced, done)
-        array = array[1:]
+	if flag.NArg() > 1 {
+		fmt.Println("Must specify only one argument - the command to map over!")
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-    for jobsFinished < numJobs {
-        <-done
-        jobsFinished++
-    }
+	if len(wordlists) < 1 {
+		clog("Must specify at least one wordlist to map over the command!")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	array := strings.Split(wordlists[0], delimiter)
+	cmd_s := flag.Arg(0)
+
+	//Small Queue.
+	done := make(chan struct{}, threads)
+	numJobs := len(array)
+	jobsFinished := 0
+	for i := 0; i < threads && len(array) > 0; i++ {
+		cmd_replaced := strings.ReplaceAll(cmd_s, replacename, array[0])
+		go runcmd(cmd_replaced, done)
+		array = array[1:]
+	}
+	for len(array) > 0 {
+		<-done
+		jobsFinished++
+		cmd_replaced := strings.ReplaceAll(cmd_s, replacename, array[0])
+		go runcmd(cmd_replaced, done)
+		array = array[1:]
+	}
+	for jobsFinished < numJobs {
+		<-done
+		jobsFinished++
+	}
 }
